ab3afb969a831737c4becb68d890e9cfebf6f565
votrepseudojulioyildogit config --global user.email votre@emailserhat.yildirim@hetic.net <serhat.yildirim@hetic.net>
2017-03-27 19:37:20 +0200

    test

--- includes/config.php
+++ includes/config.php
@@ -1,21 +1,22 @@
 <?php
+//define("URL", "http://localhost/projet_hetic_0603/"); //Url of the root
 
-// Connexion variables
-define('DB_HOST','localhost');
-define('DB_NAME','t2_partiel');
-define('DB_USER','root');
-define('DB_PASS','root'); // '' par défaut sur windows
+define('DB_HOST','localhost'); // Data-base hosting
+define('DB_NAME','spaceData'); //name of data-base
+define('DB_USER','root'); //User name
+define('DB_PASS',''); // '' Default password on phpMyadmin for Windows
 
 try
 {
-    // Try to connect to database
-    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	// Try to connect to database
+	$pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
 
-    // Set fetch mode to object
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+	// Set fetch mode to object
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ); // permet de ne pas dupliquer les résultats
+	die("ok");
 }
 catch (Exception $e)
 {
-    // Failed to connect
-    die('Could not connect');
+	// Failed to connect
+	die('Could not connect');
 }
