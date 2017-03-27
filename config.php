2bd1e4edf78a97e42f61620e0fc489638509d3ed
PaulAndrianoff <pandrianoff@gmail.com>
2017-03-27 16:18:42 +0200

    inialiasation

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+//define("URL", "http://localhost/projet_hetic_0603/"); //Url of the root
+
+//define('DB_HOST','localhost'); // Data-base hosting
+//define('DB_NAME','numismate'); //name of data-base
+//define('DB_USER','root'); //User name
+//define('DB_PASS',''); // '' Default password on phpMyadmin for Windows
+//
+//try
+//{
+//    // Try to connect to database
+//    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+//
+//    // Set fetch mode to object
+//    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ); // permet de ne pas dupliquer les résultats
+//}
+//catch (Exception $e)
+//{
+//    // Failed to connect
+//    die('Could not connect');
+//}
