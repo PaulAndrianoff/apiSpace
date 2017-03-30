900ba14435f19fa526a07c988e9dcab1486e4e8f
votrepseudojulioyildogit config --global user.email votre@emailserhat.yildirim@hetic.net <serhat.yildirim@hetic.net>
2017-03-30 14:08:10 +0200

    add stats.php animmated

--- views/pages/stats.php
+++ views/pages/stats.php
@@ -58,14 +58,15 @@
         </div>
     </div>
 
-</body>
 <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="../../src/js/jquery.vmap.js"></script>
 <script type="text/javascript" src="../../src/js/jquery.vmap.world.js" charset="utf-8"></script>
 <script type="text/javascript" src="../../src/js/jquery.vmap.sampledata.js"></script>
 <script type="text/javascript" src="../../src/js/animation.js"></script>
 
+
 <script type="text/javascript">
+<<<<<<< HEAD
 //JSON.parse permet de convertir une chaine de caractère en tableau JSON
 var agency  = JSON.parse('<?php echo json_encode($agency) ;?>');
 var countries  = JSON.parse('<?php echo json_encode($location) ;?>');
@@ -77,4 +78,18 @@ var status  = JSON.parse('<?php echo json_encode($status) ;?>');
 console.log(status);
 </script>
 
+=======
+		var agency  = JSON.parse('<?php echo json_encode($agency) ;?>');
+		var countries  = JSON.parse('<?php echo json_encode($location) ;?>');
+		var launches  = JSON.parse('<?php echo json_encode($launch) ;?>');
+		var missions  = JSON.parse('<?php echo json_encode($mission) ;?>');
+		var pads  = JSON.parse('<?php echo json_encode($pad) ;?>');
+		var rockets  = JSON.parse('<?php echo json_encode($rocket) ;?>');
+		var status  = JSON.parse('<?php echo json_encode($status) ;?>');
+</script>
+
+<script type="text/javascript" src="../../src/js/main.js"></script>
+
+</body>
+>>>>>>> 8c2d59b2ab891ca7a4261b897eadd91d2286ffe4
 </html>
