710728f425f43b0f589da8f334c94a8c9c73c6d0
PaulAndrianoff <pandrianoff@gmail.com>
2017-03-30 18:40:41 +0200

    conflic resolve

--- views/pages/stats.php
+++ views/pages/stats.php
@@ -1,76 +1,86 @@
 
 <?php
-include_once "../../includes/config.php";
-include_once "../../api_data_retrieve.php";
+ include_once "../../includes/config.php";
+ include_once "../../api_data_retrieve.php";
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Space Stats</title>
-		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<link rel="stylesheet" href="../../src/build/reset.min.css">
-		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="../../src/build/style.min.css">
-		<link rel="stylesheet" href="../../src/build/graph_style.css">
-	</head>
-	<body>
-		<div class="container-stats">
-			<header>
-				<h1 class="title-stats">SPACE STATS</h1>
-				<p class="paragraph-stats-presentation">
-					CHECK OUT THE CRITERIA OF YOUR CHOICE BY NAVIGATING ON THE WORLD MAP
-				</p>
-			</header>
+<head>
+    <meta charset="UTF-8">
+    <title>Space Stats</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="../../src/build/reset.min.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="../../src/build/style.min.css">
+    <link rel="stylesheet" href="../../src/build/graph_style.css">
+</head>
+<body>
+    <div class="container-stats">
+        <header>
+            <h1 class="title-stats">SPACE STATS</h1>
+            <p class="paragraph-stats-presentation">
+                CHECK OUT THE CRITERIA OF YOUR CHOICE BY NAVIGATING ON THE WORLD MAP
+            </p>
+        </header>
 
-			<div class="main">
-				<section class="section-map">
-					<div id="vmap" class="map"></div>
-				</section>
-				<section class="section-right">
-					<article class="agency agency-one">
-						<div class="description-agency">
-							<h3>agency a</h3>
-						</div>
-						<div class="graphic-agency">
-							<figure class="chart" data-percent="75">
-								<figcaption>graph1</figcaption>
-								<svg width="200" height="200">
-									<circle class="outer" cx="95" cy="95" r="85" transform="rotate(-90, 95, 95)"/>
-								</svg>
-							</figure>
-						</div>
+        <div class="main">
+            <section class="section-map">
+                <div id="vmap" class="map"></div>
+            </section>
+            <section class="section-right">
+                <article class="agency agency-one">
+                    <div class="description-agency">
+                        <h3 class="title_agence">Nasa</h3>
+                        <div class="agency-name"><p>Name :</p></div>
+                        <div class="creation-date"><p>Date of creation :</p></div>
+                        <div class="creation-date"><p>Annual budget :</p></div>
+                        <div class="missions-number"><p>Mission number :</p></div>
+                        <div class="pad-name"><p>Pad number :</p></div>
+                        <div class="pad-name"><p>Geographic coordinate :</p></div>
+                        <div class="pad-name"><p>Gravity :</p></div>
+                        <div class="number-rocket-sent">Number of launched :</div>
+                    </div>
+                    <div class="graphic-agency">
+                        <h4 class="title_agence">Title-graphic</h4>
+                    </div>
+                </article>
+                <article class="agency agency-two">
+                    <div class="description-agency">
+                        <h3 class="title_agence">Nasa 2</h3>
+                        <div class="agency-name"><p>Name :</p></div>
+                        <div class="creation-date"><p>Date of creation :</p></div>
+                        <div class="creation-date"><p>Annual budget :</p></div>
+                        <div class="missions-number"><p>Mission number :</p></div>
+                        <div class="pad-name"><p>Pad number :</p></div>
+                        <div class="pad-name"><p>Geographic coordinate :</p></div>
+                        <div class="pad-name"><p>Gravity :</p></div>
+                        <div class="number-rocket-sent">Number of launched :</div>
+                    </div>
+                    <div class="graphic-agency">
+                        <h4 class="title_agence">Title-graphic</h4>
+                    </div>
 
-					</article>
-					<article class="agency agency-two">
-						<div class="description-agency">
-							<h3>agency a</h3>
-						</div>
-						<div class="graphic-agency">
-						</div>
+                </article>
+            </section>
+        </div>
+    </div>
 
-					</article>
-				</section>
-			</div>
-		</div>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../../src/js/jquery.vmap.js"></script>
+    <script type="text/javascript" src="../../src/js/jquery.vmap.world.js" charset="utf-8"></script>
+    <script type="text/javascript" src="../../src/js/jquery.vmap.sampledata.js"></script>
 
-		<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-		<script type="text/javascript" src="../../src/js/jquery.vmap.js"></script>
-		<script type="text/javascript" src="../../src/js/jquery.vmap.world.js" charset="utf-8"></script>
-		<script type="text/javascript" src="../../src/js/jquery.vmap.sampledata.js"></script>
+     <script type="text/javascript">
+    var agency  = JSON.parse('<?php echo json_encode($agency) ;?>');
+    var countries  = JSON.parse('<?php echo json_encode($location) ;?>');
+    var launches  = JSON.parse('<?php echo json_encode($launch) ;?>');
+    var missions  = JSON.parse('<?php echo json_encode($mission) ;?>');
+    var pads  = JSON.parse('<?php echo json_encode($pad) ;?>');
+    var rockets  = JSON.parse('<?php echo json_encode($rocket) ;?>');
+    var status  = JSON.parse('<?php echo json_encode($status) ;?>');
+</script>
 
-		<script type="text/javascript">
-			var agency  = JSON.parse('<?php echo json_encode($agency) ;?>');
-			var countries  = JSON.parse('<?php echo json_encode($location) ;?>');
-			var launches  = JSON.parse('<?php echo json_encode($launch) ;?>');
-			var missions  = JSON.parse('<?php echo json_encode($mission) ;?>');
-			var pads  = JSON.parse('<?php echo json_encode($pad) ;?>');
-			var rockets  = JSON.parse('<?php echo json_encode($rocket) ;?>');
-			var status  = JSON.parse('<?php echo json_encode($status) ;?>');
-		</script>
+<script type="text/javascript" src="../../src/js/main.js"></script> 
 
-		<script type="text/javascript" src="../../src/js/main.js"></script>
-
-
-	</body>
+</body>
 </html>
